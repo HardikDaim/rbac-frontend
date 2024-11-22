dcb2beeede1645bd6d7a3c2f53db5cf75802ba36
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-11-22 20:08:15 +0530

    fix production endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_PROD_URL // Production URL
+  : process.env.REACT_APP_LOCAL_URL; // Local URL
 
 
 export const getUsers = async () => axios.get(`${BASE_URL}/users`);
